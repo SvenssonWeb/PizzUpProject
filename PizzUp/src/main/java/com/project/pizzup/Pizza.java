c0e2ca3ea656cb70e4fcda1783c6bb6d716da410
Fredrik Svensson <github@svenssonweb.se>
2013-11-13 14:45:15 +0100

    Added Pizzeria and Pizza Classes

---
+++ PizzUp/src/main/java/com/project/pizzup/Pizza.java
@@ -0,0 +1,13 @@
+package com.project.pizzup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Artwar on 11/13/13.
+ */
+public class Pizza {
+	public String name;
+	public double price;
+	public int rating;
+	public ArrayList<String> ingredients;
+}
