3f380eb29f6cec7811030033d49f7d789408057b
Hilda Rytteke <hilda.rytteke@gmail.com>
2013-12-03 15:14:10 +0100

    Admin start screen

---
+++ PizzUp/src/main/java/com/project/pizzup/AdminPizzaActivity.java
@@ -0,0 +1,17 @@
+package com.project.pizzup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Hilda on 2013-12-03.
+ */
+public class AdminPizzaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+
+    }
+}
