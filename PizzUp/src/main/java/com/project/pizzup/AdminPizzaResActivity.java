60c439bf7c10cf36b8177682508893f49d131352
Cissi87 <plutty87@gmail.com>
2013-12-11 14:54:03 +0100

    new
    hippo! :P

--- PizzUp/src/main/java/com/project/pizzup/AdminPizzaResActivity.java
+++ PizzUp/src/main/java/com/project/pizzup/AdminPizzaResActivity.java
@@ -1,7 +1,38 @@
 package com.project.pizzup;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.project.pizzup.Objects.Ingredient;
+import com.project.pizzup.Objects.Pizzeria;
+
+import java.util.List;
+
 /**
  * Created by cissi on 2013-12-11.
  */
-public class AdminPizzaResActivity {
+public class AdminPizzaResActivity extends Activity{
+    DataBaseHelper db;
+    ListView pizzeria;
+    Button saveBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_pizza_res);
+        db = MainActivity.myDbHelper;
+
+        pizzeria = (ListView) findViewById(R.id.pizzaAddPizzeria);
+        List<Pizzeria> restaurants = db.getAllRestaurants();
+        ArrayAdapter<Pizzeria> adapter = new ArrayAdapter<Pizzeria>(this, android.R.layout.simple_list_item_1, restaurants);
+        pizzeria.setAdapter(adapter);
+
+        saveBtn = (Button) findViewById(R.id.saveBtn);
+
+    }
 }
