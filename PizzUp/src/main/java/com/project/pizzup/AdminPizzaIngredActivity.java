a46d755f2a25a6160bd729b9e0e1a3c7554bfe2b
Hilda Rytteke <hilda.rytteke@gmail.com>
2013-12-17 11:02:53 +0100

    Admin - add pizza - add ingredients

--- PizzUp/src/main/java/com/project/pizzup/AdminPizzaIngredActivity.java
+++ PizzUp/src/main/java/com/project/pizzup/AdminPizzaIngredActivity.java
@@ -1,7 +1,9 @@
 package com.project.pizzup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -9,6 +11,7 @@ import android.widget.ListView;
 
 import com.project.pizzup.Objects.Ingredient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +21,7 @@ public class AdminPizzaIngredActivity extends Activity implements View.OnClickLi
     DataBaseHelper db;
     ListView ingredients;
     Button saveBtn;
+    ArrayAdapter<Ingredient> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,7 @@ public class AdminPizzaIngredActivity extends Activity implements View.OnClickLi
 
         ingredients = (ListView) findViewById(R.id.pizzaAddIngredient);
         List<Ingredient> ingredient = db.getAllIngredients();
-        ArrayAdapter<Ingredient> adapter = new ArrayAdapter<Ingredient>(this, android.R.layout.simple_list_item_multiple_choice, ingredient);
+        adapter = new ArrayAdapter<Ingredient>(this, android.R.layout.simple_list_item_multiple_choice, ingredient);
         ingredients.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         ingredients.setAdapter(adapter);
 
@@ -37,6 +41,24 @@ public class AdminPizzaIngredActivity extends Activity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
+        SparseBooleanArray checked = ingredients.getCheckedItemPositions();
+        ArrayList<Ingredient> selectedItems = new ArrayList<Ingredient>();
+        for (int i = 0; i < checked.size(); i++) {
+            int position = checked.keyAt(i);
+            if (checked.valueAt(i))
+                selectedItems.add(adapter.getItem(position));
+        }
+
+        String temp ="";
+        for (Ingredient i : selectedItems) {
+            temp += i.id+",";
+        }
+
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(AdminPizzaActivity.INGRED, temp);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
 
     }
 }
